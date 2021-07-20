7140eecfcbd64d752cff141318516ca301fe4ff9
VARSHAC11 <85901564+VARSHAC11@users.noreply.github.com>
2021-07-20 15:45:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -38,12 +38,13 @@ function createUserCard(user) {
               <img class="avatar" src="${user.avatar_url}" alt="${user.name}" />
           </div>
           <div class="user-info">
-            
-             
-            <h2>${user.name}</h2>
+              <h2>${user.name}</h2>
               <p>${user.bio}</p>
               <ul class="info">
+                  <li><strong>Followers :</strong>${user.followers}</li>
+                  <li><strong>Following :</strong>${user.following}</li>
                   <li><strong>Repos :</strong>${user.public_repos}</li>                  
+                  <li><strong>Location :</strong>${user.location}</li>
               </ul>
               <div id="repos"></div>
           </div>
